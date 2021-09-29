ad408652569877866850842cfa5ca0b371e6edff
Adrien Guard <56919132+Adrienguard@users.noreply.github.com>
2021-09-29 17:36:58 +0200

    clean parser

--- index.js
+++ index.js
@@ -11,35 +11,3 @@ const  deprecatedDirective  = require("./lib/deprecatedDirective");
 
 const { getDirectives} = require( "@graphql-tools/utils")
 
-//const { deprecatedDirectiveTypeDefs, deprecatedDirectiveTransformer } = deprecatedDirective('deprecated');
-
-const schemaCode = fs.readFileSync(path.join(__dirname, './graphql-examples', 'blog.graphql'), 'utf8')
-
-
-const chegue = `
-type ExampleType {
-  newField: String
-  oldField: String @deprecated(reason: "Use \`newField\`.")
-}
-
-type Query {
-  rootField: ExampleType
-}
-`
-
-
-
-const schema = easygraphqlSchemaParser(schemaCode)
-const schemaTransform = deprecatedDirective('deprecated');
-//const newSchema = schemaTransform.deprecatedDirectiveTransformer(chegue);
-
-
-
-
-//console.log(getDirectives(chegue))
-fs.writeFile("schema.json", JSON.stringify(schema, null, 4),(err) => {
-    if (err) {
-        throw err;
-    }
-    console.log("JSON data is saved.");
-});
