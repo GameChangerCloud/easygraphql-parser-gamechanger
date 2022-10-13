9e95706b43e38629af3d8b7f608b3c4321f7eb55
cfaboumyIppon <114093660+cfaboumyIppon@users.noreply.github.com>
2022-10-13 12:03:51 +0200

    Add relation field (relatedFieldName) + manage join annotation
    + start work on enum handling

--- lib/models/type.ts
+++ lib/models/type.ts
@@ -9,6 +9,7 @@ export interface IType {
     description: string;
     directives: any[];
     relationList: any[];
+    values: string[];
     fields: Field[];
     implementedTypes: any;
 }
@@ -20,15 +21,17 @@ export class Type {
     description: string;
     directives: any[];
     relationList: any[];
+    values: string[];
     fields: Field[];
     implementedTypes: any;
 
-    constructor(type: string, typeName: string, sqlTypeName: string, description: string, directives: any[], implementedTypes: any[],) {
+    constructor(type: string, typeName: string, sqlTypeName: string, description: string, values: string[], directives: any[], implementedTypes: any[],) {
         this.type = type
         this.typeName = typeName
         this.sqlTypeName = sqlTypeName
         this.description = description
         this.directives = directives
+        this.values = values
         this.fields = []
         this.implementedTypes = implementedTypes
         // stores all the relations which were found for a give type
@@ -43,13 +46,15 @@ export class Type {
                 typeName,
                 getSQLTableName(typeName),
                 schemaJSON[typeName].description ? schemaJSON[typeName].description : "",
+                schemaJSON[typeName].values ? schemaJSON[typeName].values : [],
                 schemaJSON[typeName].directives ? schemaJSON[typeName].directives : [],
-                schemaJSON[typeName].implementedTypes ? schemaJSON[typeName].implementedTypes : []
+                schemaJSON[typeName].implementedTypes ? schemaJSON[typeName].implementedTypes : [],
             )
             for (const field of schemaJSON[typeName].fields as IField[]) {
                 let fieldToAdd = new Field(
                     field.name,
                     field.type,
+                    field.isEnum,
                     field.noNullArrayValues,
                     field.noNull,
                     field.isArray,
@@ -67,6 +72,16 @@ export class Type {
             }
             types.push(typeToAdd)
         }
+
+        types.filter(type => type.type === "ObjectTypeDefinition" && type.isNotOperation())
+        .forEach(currentType => {
+            currentType.fields.forEach(field => {
+                if(types.find(typeE => field.type === typeE.typeName && typeE.type === "EnumTypeDefinition"))
+                field.isEnum = true;
+            })
+
+        });
+
         return types;
     }
 
